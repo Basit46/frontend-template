d889defdc1664a1a85ce4d779fd41cfa5be2edaa
basit <hassanbasit1990@gmail.com>
2024-07-27 12:57:54 +0100

    Added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="text-white">
-      <Home />
+    <div>
+      <Navbar />
     </div>
   );
 }
